747201ffce643bcd26ade15c5775ed8d58e058e7
sayan1019 <arpankarak92@gmail.com>
2022-04-23 11:25:12 +0530

    js append problem fix

--- js/brain.js
+++ js/brain.js
@@ -16,13 +16,14 @@ li.forEach(element => {
 const mediaQuery = window.matchMedia('(max-width: 900px)');
 
 function handleTabletChange(e) {
+    let uploadBtn = document.querySelector('#button');
     if (e.matches) {
-        let uploadBtn = document.querySelector('#button');
-        console.log(uploadBtn);
         let navBar = document.querySelector("#navbar ul");
         navBar.appendChild(uploadBtn);
+    } else{
+        let header = document.querySelector("#head");
+        header.appendChild(uploadBtn);
     }
 }
-
 mediaQuery.addListener(handleTabletChange);
 handleTabletChange(mediaQuery);   
