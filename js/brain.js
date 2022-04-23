b364a8e7747fbb23680082a348d8cf8ae95add38
sayan1019 <arpankarak92@gmail.com>
2022-04-23 10:45:59 +0530

    css menu bar updated

--- js/brain.js
+++ js/brain.js
@@ -2,12 +2,27 @@
 
 let li = document.querySelectorAll('li');
 
-li.forEach(element=> {
-    element.onclick = ()=> {
-        li.forEach(element=>{
+li.forEach(element => {
+    element.onclick = () => {
+        li.forEach(element => {
             element.classList.remove('active');
         })
         element.classList.add('active');
     };
 })
 
+//media query target
+
+const mediaQuery = window.matchMedia('(max-width: 900px)');
+
+function handleTabletChange(e) {
+    if (e.matches) {
+        let uploadBtn = document.querySelector('#button');
+        console.log(uploadBtn);
+        let navBar = document.querySelector("#navbar ul");
+        navBar.appendChild(uploadBtn);
+    }
+}
+
+mediaQuery.addListener(handleTabletChange);
+handleTabletChange(mediaQuery);   
