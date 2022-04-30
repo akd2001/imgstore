28aa8d933dd14895504b8e3500cbad253929d98c
arnab kumar das <electroarnab2001@gmail.com>
2022-04-30 12:11:34 +0530

    add text,email input validation

--- js/script.js
+++ js/script.js
@@ -5,11 +5,26 @@
 *    © ARNAB KUMAR DAS    © electroarnab2001@gmail.com    https://github.com/akd2001
 */
 
+// form input validation 
+function validate(inputField, userReg, icon) {
+    inputField.oninput = function valid() {
+        if (userReg.test(inputField.value)) {
+            icon.style.color = "#27d027";
+        } else {
+            icon.style.color = "#ff0000";
+        }
+    };
+}
+//check signin-user-name
+validate(document.getElementById('loginUsername'), /^[A-Za-z]([a-zA-Z0-9\s+]){4,20}(?!\s+)$/, document.getElementById("login-user-name-icon"));
+//check signup-user-name
+validate(document.getElementById('signupUsername'), /^[A-Za-z]([a-zA-Z0-9\s+]){4,20}(?!\s+)$/, document.getElementById("signup-user-name-icon"));
+//check signup-user-email
+validate(document.getElementById("signupEmail"), /^([a-zA-Z0-9\.\-]+)@gmail\.com$/, document.getElementById("signup-user-email-icon"));
+
 
 //navbar active class
-
 let li = document.querySelectorAll('li');
-
 li.forEach(element => {
     element.onclick = () => {
         li.forEach(element => {
@@ -47,8 +62,8 @@ function handleMobileChange(e) {
 }
 mediaOne.addListener(handleTabletChange);
 mediaTwo.addListener(handleMobileChange);
-handleTabletChange(mediaOne);   
-handleMobileChange(mediaTwo);   
+handleTabletChange(mediaOne);
+handleMobileChange(mediaTwo);
 
 // popup open and close 
 
@@ -57,7 +72,7 @@ let popup = document.getElementById("formSection");
 // openpopup
 
 let openpopBtn = document.getElementById('openpopBtn');
-openpopBtn.onclick = ()=> {
+openpopBtn.onclick = () => {
     popup.style.display = "flex";
 }
 
@@ -65,6 +80,6 @@ openpopBtn.onclick = ()=> {
 
 let closepopBtn = document.getElementById("closeForm");
 
-closepopBtn.onclick = ()=> {
+closepopBtn.onclick = () => {
     popup.style.display = "none";
 }
